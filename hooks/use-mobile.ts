5276b222e8544316e11dec7be1ac2ea6335ea9e2
SANN GROUP <stokakunsanzz250@gmail.com>
2026-06-30 01:48:42 +0700

    Upload hooks/use-mobile.ts

---
+++ hooks/use-mobile.ts
@@ -0,0 +1,21 @@
+import * as React from "react"
+
+const MOBILE_BREAKPOINT = 768
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState<boolean>(false)
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    }
+    
+    // Set initial value
+    handleResize()
+    
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return isMobile
+}
